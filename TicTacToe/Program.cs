7cecfb74f7a14cc6abcc9cd6570973ace8d62758
Damian Pękala <damian.pekala@microsoft.wsei.edu.pl>
2019-05-27 14:04:04 +0200

    Added a loop

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -9,12 +9,36 @@ namespace TicTacToe
             string playerNameB = "";
             char playerSymbolA = 'x';
             char playerSymbolB = 'o';
-            char[,] board = new char[3, 3];
+            char[,] board = new char[3, 3] {
+ { '1', '2', '3' },
+ { '4', '5', '6' },
+ { '7', '8', '9' }
+ };
             Console.Write("Enter the name of player A: ");
             playerNameA = Console.ReadLine();
             Console.Write("Enter the name of player B: ");
             playerNameB = Console.ReadLine();
-            // ...
+            // A loop over players moves
+            bool gameOver = false;
+            while (!gameOver)
+            {
+                Console.Clear();
+                DrawBoard(board);
+                Console.ReadKey();
+                // ...
+            }
+        }
+        ////////////////////////////////////////////////
+        static void DrawBoard(char[,] board)
+        {
+            int height = board.GetLength(0);
+            int width = board.GetLength(1);
+            for (int i = 0; i < height; ++i)
+            {
+                for (int j = 0; j < width; ++j)
+                    Console.Write(board[i, j]);
+                Console.WriteLine();
+            }
         }
     }
 }
