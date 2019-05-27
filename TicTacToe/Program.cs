dabaf35decfb486beb89fd638acc10ce9b1472cd
Damian Pękala <damian.pekala@microsoft.wsei.edu.pl>
2019-05-27 14:02:23 +0200

    First code

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -1,15 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace TicTacToe
 {
     class Program
     {
         static void Main(string[] args)
         {
+            string playerNameA = "";
+            string playerNameB = "";
+            char playerSymbolA = 'x';
+            char playerSymbolB = 'o';
+            char[,] board = new char[3, 3];
+            Console.Write("Enter the name of player A: ");
+            playerNameA = Console.ReadLine();
+            Console.Write("Enter the name of player B: ");
+            playerNameB = Console.ReadLine();
+            // ...
         }
     }
 }
